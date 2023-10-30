a9f7ee3ff4a2528cc3ba11639f5d062d98cf8938
kira607 <kirill.lesckin@gmail.com>
2023-10-30 17:01:46 +0100

    removed ecs package

--- src/space_ranger/core/ecs/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Entity Component System base classes."""
-
-from .component import Component as Component
-from .ecs_manager import EcsManager as EcsManager
-from .entity import Entity as Entity
-from .system import System as System
